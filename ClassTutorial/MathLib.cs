4f86d9a161c83267c91e9c1d44f11f27636d6b87
Charlie Ford <charles.ford1978@gmail.com>
2021-09-27 16:36:05 -0400

    Add project files.

---
+++ ClassTutorial/MathLib.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTutorial
+{
+    class MathLib
+    {
+        public MathLib()
+        {
+
+        }
+        public MathLib(int nbr1)
+        {
+
+        }
+        public int Mod(int nbr1, int nbr2)
+        {
+            var rem = 0;
+            while (nbr1 - nbr2 > nbr2)
+            {
+                nbr1 = nbr1 - nbr2;            
+            }             
+            rem = nbr1 - nbr2;
+            
+            return rem;
+        }
+        public int Inc(int nbr1)
+        {
+            return nbr1 + 1;
+        }
+        
+        public int Add(int nbr1, int nbr2)
+        {
+            return nbr1 + nbr2;
+        }
+        public int Sub(int nbr1, int nbr2)
+        {
+            return nbr1 - nbr2;
+        }
+        public int Mult(int nbr1, int nbr2)
+        {
+            return nbr1 * nbr2;
+        }
+        public int Div(int nbr1, int nbr2)
+        {
+            return nbr1 / nbr2;
+        }
+    }
+}
---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassTutorial {
+    class Program {
+        static void Main(string[] args) {
+
+            var fred = new Student();
+            fred.Name = "Fred";
+            fred.SAT = 1000;
+            var wilma = new Student();
+            wilma.Name = "Wilma";
+            wilma.SAT = 2000;
+            
+            var message = wilma.Print(1);
+            Console.WriteLine(message);
+
+            message = fred.Print(2);
+            Console.WriteLine(message);
+
+            var barney = new Student();
+            barney.Name = "Barney";
+            barney.SAT = 1200;
+            wilma.Print(1);
+            fred.Print(2);
+            barney.Print(3);
+            barney.SAT = barney.SAT + 20;
+            barney.Print(4);
+
+            var mathlib = new MathLib();
+            var nbr1 = 10;
+            var nbr2 = 4;
+            var result = mathlib.Add(nbr1, nbr2);
+            Console.WriteLine($"The sum is {result}");
+            result = mathlib.Sub(nbr1, nbr2);
+            Console.WriteLine($"The difference is {result}");
+            result = mathlib.Mult(nbr1, nbr2);
+            Console.WriteLine($"The product is {result}");
+            result = mathlib.Div(nbr1, nbr2);
+            Console.WriteLine($"The quotient is {result}");
+            result = mathlib.Mod(nbr1, nbr2);
+            Console.WriteLine($"The remainder is {result}");
+        }
+    }
+}
